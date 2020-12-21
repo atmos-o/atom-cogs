dc96fd468c9066fca20240a853283779cf0d2d91
Atmos <44858581+atmos-o@users.noreply.github.com>
2020-12-21 09:05:25 +0000

    <:YEPHIGH:757053149684170862>

--- RandoCocks/mycog.py
+++ RandoCocks/mycog.py
@@ -2,29 +2,26 @@ from redbot.core import commands
 from redbot.core.bot import Red
 import random
 import datetime
+from redbot.core import Config
 
 class RandoCocks(commands.Cog):
   def __init__(self, bot: Red):
     self.bot = bot
+    self.config = Config.get_conf(self, identifier=1027)
+    self.config.register_global(
+      conf_current_month = 13,
+      conf_current_seed = 1304
+    )
+
     self.middle_list = ['has', 'is packing', 'is endowed with', 'wields', 'is hiding']
     self.ending_list = ['rock hard steel.', 'a baby-making wand.', 'waifu-destroying power. OwO', 'man meat. COCKADOODLEDOO!!', 'a manly turkey baster.', 'chocolate stuffing power.', 'GACHI STEAK!']
 
   @commands.command()
   async def cock(self, ctx):
     # get seed and month
-    data_file_path = 'data.txt'
-    try:
-      data_file = open(data_file_path, 'r')
-    except:
-      data_file = open(data_file_path, 'w')
-      data_file.write('12 1308')
-      data_file.close()
-      data_file = open(data_file_path, 'r')
-    saved_data = data_file.readline()
-    saved_data_list = saved_data.split()
-    saved_month = saved_data_list[0]
-    saved_seed = int(saved_data_list[1])
-    data_file.close()
+    saved_month = await self.config.conf_current_month()
+    saved_seed = await self.config.conf_current_seed()
+    #print(saved_month, saved_seed)
 
     # check for re-roll
     now = str(datetime.datetime.now())
@@ -32,9 +29,10 @@ class RandoCocks(commands.Cog):
     new_month = temp_list[1]
     if new_month != saved_month:
       new_seed = random.randint(1, 10000)
-      data_file = open(data_file_path, 'w')
-      data_file.write(new_month + ' ' + str(new_seed))
-      data_file.close()
+      #TODO: save new seed
+      await self.config.conf_current_month.set(new_month)
+      await self.config.conf_current_seed.set(new_seed)
+
       saved_seed = new_seed
       await ctx.send('NEW RANDOM SEED ROLL!!!! CHECK OUT YOUR NEW SIZE FOR THE MONTH!!!!')
 
@@ -46,3 +44,4 @@ class RandoCocks(commands.Cog):
       length_string = length_string[: -2]
     random.seed()
     await ctx.send(ctx.author.name + ' ' + self.middle_list[random.randint(0, len(self.middle_list) - 1)] + ' ' + length_string + ' inches of ' + self.ending_list[random.randint(0, len(self.ending_list) - 1)])
+    #print(new_month, saved_seed)
